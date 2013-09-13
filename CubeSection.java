0304b2a4045f481ec259881939dd6405fca8228d
viksree <viksree@gmail.com>
2013-09-12 21:17:21 -0700

    Remove old files

--- CubeSection.java
+++
@@ -1,8 +0,0 @@
-public enum CubeSection {
-  LEFT("left"), 
-  RIGHT("right"),
-  TOP("top"),
-  BOTTOM("bottom"),
-  VERTICAL_MIDDLE("vertical_middle"),
-  HORIZONTAL_MIDDLE("horizonal_middle");
-}
