828ddd50bb880d381d7fd3d464e97de35f67c9a3
viksree <viksree@gmail.com>
2013-09-12 20:56:20 -0700

    Add CubeSection enum

---
+++ CubeSection.java
@@ -0,0 +1,8 @@
+public enum CubeSection {
+  LEFT("left"), 
+  RIGHT("right"),
+  TOP("top"),
+  BOTTOM("bottom"),
+  VERTICAL_MIDDLE("vertical_middle"),
+  HORIZONTAL_MIDDLE("horizonal_middle");
+}
